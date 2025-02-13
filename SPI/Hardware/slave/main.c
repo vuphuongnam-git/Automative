0c1f93dd4460e587361464c91e0098cc7a006f96
Nam-Vu <pigbabyclgt@gmail.com>
2025-02-13 20:58:00 +0700

    spi_hardware

---
+++ SPI/Hardware/slave/main.c
@@ -0,0 +1,96 @@
+#include "stm32f10x.h"                  // Device header
+#include "stm32f10x_gpio.h"             // Keil::Device:StdPeriph Drivers:GPIO
+#include "stm32f10x_rcc.h"              // Keil::Device:StdPeriph Drivers:RCC
+#include "stm32f10x_tim.h"              // Keil::Device:StdPeriph Drivers:TIM
+#include "stm32f10x_spi.h"
+
+#define SPI1_NSS 		GPIO_Pin_4
+#define SPI1_SCK		GPIO_Pin_5
+#define SPI1_MISO 	GPIO_Pin_6
+#define SPI1_MOSI 	GPIO_Pin_7
+#define SPI1_GPIO 	GPIOA
+
+
+void RCC_Config(){
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA | RCC_APB2Periph_SPI1, ENABLE);
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2, ENABLE);
+}
+
+void GPIO_Config(){
+	GPIO_InitTypeDef GPIO_InitStructure;
+	
+	GPIO_InitStructure.GPIO_Pin = SPI1_NSS | SPI1_SCK | SPI1_MISO | SPI1_MOSI;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
+
+	GPIO_Init(SPI1_GPIO, &GPIO_InitStructure);
+}
+
+void SPI_Config(){
+	SPI_InitTypeDef SPI_InitStructure;
+	SPI_InitStructure.SPI_Mode = SPI_Mode_Slave;
+	SPI_InitStructure.SPI_Direction = SPI_Direction_2Lines_FullDuplex;
+	SPI_InitStructure.SPI_BaudRatePrescaler = SPI_BaudRatePrescaler_16;
+	SPI_InitStructure.SPI_CPOL = SPI_CPOL_Low;
+	SPI_InitStructure.SPI_CPHA = SPI_CPHA_1Edge;
+	SPI_InitStructure.SPI_DataSize = SPI_DataSize_8b;
+	SPI_InitStructure.SPI_FirstBit = SPI_FirstBit_LSB;
+	SPI_InitStructure.SPI_CRCPolynomial = 7;
+	SPI_InitStructure.SPI_NSS = SPI_NSS_Soft;
+	
+	SPI_Init(SPI1, &SPI_InitStructure);
+	SPI_Cmd(SPI1, ENABLE);
+}
+
+void SPI_Send1Byte(uint8_t data){
+GPIO_ResetBits(GPIOA, SPI1_NSS);
+while(SPI_I2S_GetFlagStatus(SPI1, SPI_I2S_FLAG_TXE) == RESET){}
+SPI_I2S_SendData(SPI1, data);
+while(SPI_I2S_GetFlagStatus(SPI1, SPI_I2S_FLAG_BSY) == SET){}
+	GPIO_SetBits(GPIOA, SPI1_NSS);
+}
+
+uint8_t SPI_Receive1Byte(void){
+    while(SPI_I2S_GetFlagStatus(SPI1, SPI_I2S_FLAG_BSY) == SET);
+    uint8_t temp = (uint8_t)SPI_I2S_ReceiveData(SPI1);
+    while(SPI_I2S_GetFlagStatus(SPI1, SPI_I2S_FLAG_RXNE) == RESET);
+    return temp;
+}
+
+
+void TIM_Config(){
+	TIM_TimeBaseInitTypeDef TIM_InitStruct;
+	
+	TIM_InitStruct.TIM_ClockDivision = TIM_CKD_DIV1;
+	TIM_InitStruct.TIM_Prescaler = 7200 - 1; // do MCU dem tu 0 len
+	TIM_InitStruct.TIM_Period = 0xFFFF;
+	TIM_InitStruct.TIM_CounterMode = TIM_CounterMode_Up;
+	
+	TIM_TimeBaseInit(TIM2, &TIM_InitStruct);
+	TIM_Cmd(TIM2, ENABLE);
+	
+}
+
+void delay_ms(uint16_t time){
+	TIM_SetCounter(TIM2, ENABLE);
+	while(TIM_GetCounter(TIM2) < time * 10);
+}
+
+
+//uint8_t dataSend[] = {3, 1, 10, 19, 20, 36, 90};
+uint8_t dataRecei[7];
+int main(){
+	RCC_Config();
+	GPIO_Config();
+	TIM_Config();
+	SPI_Config();
+	while(1){
+		for(int i = 0; i < 7; i++){
+			dataRecei[i] = SPI_Receive1Byte();
+			delay_ms(1000);
+		}
+	}
+}
+
+
+
